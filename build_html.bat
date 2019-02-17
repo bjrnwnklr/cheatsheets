e391e5582ccdd359c5ca06f19ada3277dd64379e
Bjoern Winkler <bjoern@bjoern-winkler.de>
2019-02-17 19:58:13 +0100

    Updated cheatsheets

--- build_html.bat
+++ build_html.bat
@@ -1,3 +1,3 @@
-pandoc cheatsheet.md -f markdown -t html -s -o "D:\Benutzer\bjoern\Google Drive\Python\cheatsheet.html" -c retro.css --highlight-style zenburn
-pandoc "AdventOfCodeCheatsheet.md" -f markdown -t html -s -o "D:\Benutzer\bjoern\Google Drive\Python\AdventOfCodeCheatsheet.html" -c retro.css --highlight-style zenburn
-pandoc "git_cheatsheet.md" -f markdown -t html -s -o "D:\Benutzer\bjoern\Google Drive\Python\git_cheatsheet.html" -c retro.css --highlight-style zenburn
+pandoc cheatsheet.md -f markdown -t html -s -o "cheatsheet.html" -c retro.css --highlight-style zenburn
+pandoc "AdventOfCodeCheatsheet.md" -f markdown -t html -s -o "AdventOfCodeCheatsheet.html" -c retro.css --highlight-style zenburn
+pandoc "git_cheatsheet.md" -f markdown -t html -s -o "git_cheatsheet.html" -c retro.css --highlight-style zenburn
