4e13643ec5741369cc66c5d2683aa8d5b7435723
Bjoern Winkler <bjoern@bjoern-winkler.de>
2019-07-07 19:59:17 +0200

    build-html.py

--- build-html.py
+++ build-html.py
@@ -1,8 +1,19 @@
 from pathlib import Path
 
-p = Path('.')
 
-files = list(p.glob('*.md'))
+if __name__ == '__main__':
 
-for f in files:
-    print(f)
+    p = Path('.')
+    ext = '*.md'
+    output_dir = p / 'html'
+    css_file = p / 'css' / 'pandoc.css'
+
+    # define pandoc command
+    pandoc_tmpl = 'pandoc {} --output {} --css {} --from markdown --to hmtl --standalone --highlight-style kate --toc --toc-depth 2'
+    
+    # get all md files
+    files = list(p.glob(ext))
+
+    for f in files:
+        pandoc_cmd = pandoc_tmpl.format(f.name, f.stem + '.html', css_file)
+        print(pandoc_cmd)
